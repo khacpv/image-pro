58ffa9cc5899a14258066f4d5ac35201d619d8eb
khacpv <khacpv@gmail.com>
2016-06-20 00:21:21 +0700

    update

--- routes/native.js
+++ routes/native.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var nativeLib = require('../native/native');
+//var nativeLib = require('../native/native');
 var router = express.Router();
 
 /* GET native */
 router.post('/', function(req, res, next) {
 
     var name = req.body.name;
-    var myResult = nativeLib.getString(name);
+    var myResult = 'test'/*nativeLib.getString(name)*/;
     res.json({value:myResult});
 });
 
