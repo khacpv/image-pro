5f97b14f272ea61f89304afe44e9f0d3bb597620
khacpham <khacpv@gmail.com>
2016-06-22 00:29:12 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Call Function C++ from NodeJS' });
 });
 
 module.exports = router;
