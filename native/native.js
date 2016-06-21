4c887915e21303c676fc3721ebdbcadfebf19cae
khacpham <khacpv@gmail.com>
2016-06-22 00:23:56 +0700

    update

--- native/native.js
+++ native/native.js
@@ -5,15 +5,15 @@ myLib = ffi.Library('./bin/myLib', {
     'factorial': ['uint64', ['int']],
     'getString': ['String',['String']]
 });
+
+if (process.argv.length < 3) {
+    console.log('default value: 5');
+}
+
+//var output = myLib.factorial(parseInt(process.argv[2]) || 5);
 //
-//if (process.argv.length < 3) {
-//    console.log('default value: 5');
-//}
+//console.log('Your output: ' + output);
 //
-////var output = myLib.factorial(parseInt(process.argv[2]) || 5);
-////
-////console.log('Your output: ' + output);
-////
-//console.log(myLib.getString("pham khac"));
+console.log(myLib.getString("pham khac"));
 
 module.exports = myLib || {};
