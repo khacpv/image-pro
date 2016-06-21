5da5ffb5a97c8befa0c6a4706741273347c6a666
khacpham <khacpv@gmail.com>
2016-06-22 00:03:16 +0700

    update

--- native/native.js
+++ native/native.js
@@ -1,10 +1,9 @@
 var ffi = require('ffi');
 
-var myLib;
-//var myLib = ffi.Library('./bin/myLib', {
-//    'factorial': ['uint64', ['int']],
-//    'getString': ['String',['String']]
-//});
+var myLib = ffi.Library('./bin/myLib', {
+    'factorial': ['uint64', ['int']],
+    'getString': ['String',['String']]
+});
 //
 //if (process.argv.length < 3) {
 //    console.log('default value: 5');
