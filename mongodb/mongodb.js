91dfb7bf533204d85467b68fa46d84e1015c7f25
khacpham <khacpv@gmail.com>
2016-06-23 00:25:14 +0700

    update

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -2,8 +2,8 @@ var mongojs = require('mongojs');
 
 var Mongo = {};
 
-Mongo.IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-Mongo.PORT = process.env.OPENSHIFT_NODEJS_PORT || '52086';
+Mongo.IP = process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1';
+Mongo.PORT = process.env.OPENSHIFT_MONGODB_DB_PORT || '52086';
 Mongo.dbName = process.env.OPENSHIFT_APP_NAME || 'image';
 Mongo.user = process.env.OPENSHIFT_MONGODB_DB_USERNAME || 'admin';
 Mongo.pass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD || 'T3k_111XG72d';
