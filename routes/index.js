05c4830a863ae03acd1817415e858d59754aa4de
khacpham <khacpv@gmail.com>
2016-07-05 00:20:40 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
 var express = require('express');
-var mongo = require('../mongodb/mongodb');
+//var mongo = require('../mongodb/mongodb');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    mongo.db.apps.find({}).limit(10).toArray(function (err, doc) {
-        if (err) {
-            res.status(500).send({err: err});
-            return;
-        }
-        if (doc && doc.length > 0) {
-            res.render('index', {title: doc[0].title, apps: doc});
-        } else {
-            res.render('index', {title: 'Call C++ func', apps: []})
-        }
-    });
+    // redirect to UPnDOWN game
+    res.redirect('/up_down');
+
+    //mongo.db.apps.find({}).limit(10).toArray(function (err, doc) {
+    //    if (err) {
+    //        res.status(500).send({err: err});
+    //        return;
+    //    }
+    //    if (doc && doc.length > 0) {
+    //        res.render('index', {title: doc[0].title, apps: doc});
+    //    } else {
+    //        res.render('index', {title: 'Call C++ func', apps: []})
+    //    }
+    //});
 });
 
 module.exports = router;
